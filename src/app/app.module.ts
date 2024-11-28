14ade2df7a3aa02a616ac296333f59f3718ed71d
pcplazash <paulo.plazas@gmail.com>
2024-11-28 16:25:15 -0500

    prueba nov 28

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+
+//import { AppRoutingModule } from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ButtonsComponent} from './buttons/buttons.component';
+import {HeaderComponent} from './header/header.component';
+import {LoginFormComponent} from './login-form/login-form.component';
+import {WelcomeContentComponent} from './welcome-message/welcome-message.component';
+import {AuthContentComponent} from './auth-content/auth-content.component';
+import {ContentComponent} from './content/content.component';
+
+import {AxiosService} from './axios.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ButtonsComponent,
+    HeaderComponent,
+    LoginFormComponent,
+    WelcomeContentComponent,
+    AuthContentComponent,
+    ContentComponent
+  ],
+  imports: [
+    BrowserModule,
+    //AppRoutingModule,
+    FormsModule
+  ],
+  providers: [AxiosService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
---
+++ src/app/welcome-message/welcome-message.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-content',
+  templateUrl: './welcome-message.component.html',
+  styleUrls: ['./welcome-message.component.css'],
+  standalone: false,
+})
+export class WelcomeContentComponent {
+
+}
